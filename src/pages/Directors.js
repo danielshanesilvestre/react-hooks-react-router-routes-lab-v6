d4028967c2d673d1963713c85152750b7b15df8b
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-29 13:47:59 -0500

    Completed lab

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then(resp => resp.json())
+      .then(data => {
+        console.log("Fetch returned!");
+        console.log(data);
+        setDirectors(data);
+      });
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map(director => {
+          return <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map(movie => {
+                return <li key={movie}>{movie}</li>
+              })}
+            </ul>
+          </article>
+        })}
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movieData, setMovieData] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${params.id}`)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log("Fetch returned!");
+        console.log(data);
+        setMovieData(data);
+      });
+  }, []);
+
+  if (movieData === null) return <p>Loading...</p>;
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movieData.title}</h1>
+        <p>{movieData.time}</p>
+        {movieData.genres.map(genre => {
+          return <span key={genre}>{genre}</span>
+        })}
       </main>
     </>
   );
